3ce15a9a4c58c2783c1eb9576e1f53c4936492ba
r1chwo0n <woonzy09@gmail.com>
2023-09-18 15:11:38 +0700

    finish

--- src/app/page.js
+++ src/app/page.js
@@ -36,10 +36,12 @@ export default function Home() {
   };
 
   const loadMyCourses = async () => {
+    setLoadingMyCourses(true);
     const resp = await axios.get("/api/enrollment", {
       headers: { Authorization: `Bearer ${token}` },
     });
     setMyCourses(resp.data.courses);
+    setLoadingMyCourses(false);
   };
 
   useEffect(() => {
@@ -54,15 +56,19 @@ export default function Home() {
 
   const login = async () => {
     try {
+      setLoadingLogin(true);
       const resp = await axios.post("/api/user/login", { username, password });
       setToken(resp.data.token);
       setAuthenUsername(resp.data.username);
       setUsername("");
       setPassword("");
+      setLoadingLogin(false);
     } catch (error) {
+      setLoadingLogin(true);
       if (error.response.data) {
         alert(error.response.data.message);
       }
+      setLoadingLogin(false);
     }
   };
 
@@ -105,7 +111,12 @@ export default function Home() {
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
               />
-              <Button onClick={login}>Login</Button>
+              {!loadingLogin && <Button onClick={login}>Login</Button>}
+              {loadingLogin && (
+                <Button onClick={login} disabled>
+                  Login...
+                </Button>
+              )}
             </Group>
           )}
           {authenUsername && (
@@ -133,9 +144,9 @@ export default function Home() {
             ))}
 
           {/* Do something with below loader!! */}
-          <Loader variant="dots" />
+          {loadingMyCourses && <Loader variant="dots" />}
         </Paper>
-        <Footer year="2023" fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer year="2023" fullName="Nawawan" studentId="650610775" />
       </Stack>
     </Container>
   );
